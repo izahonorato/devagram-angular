15abf82bd313f8cbf58e4b3339fd958d66234667
Izaura <izahonorato@gmail.com>
2022-10-01 16:18:29 -0300

    criado modulo de botao

---
+++ src/app/compartilhado/compartilhado.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BotaoComponent } from './botao/botao.component';
+
+
+
+@NgModule({
+  declarations: [
+    BotaoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CompartilhadoModule { }
